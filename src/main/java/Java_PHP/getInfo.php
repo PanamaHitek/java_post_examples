a9692c9e122eaa3ce11f0bf6d3a0c0531386eb43
Antony García <antony.garcia.gonzalez@gmail.com>
2017-04-24 10:01:14 -0500

    Finalizados los detalles sobre ejemplos del uso de PHP y Java

--- src/main/java/Java_PHP/getInfo.php
+++ src/main/java/Java_PHP/getInfo.php
@@ -1,24 +1,28 @@
 <?php
+//Información y credenciales de base de datos
 $server = "localhost";
-$usuario = "root";
+$user = "root";
 $pass = "";
-$BD = "example_database";
-$conection = mysqli_connect($server, $usuario, $pass, $BD);
+$database = "example_database";
+//Conexión a la base de datos
+$conection = mysqli_connect($server, $user, $pass, $database);
 if (!$conection) {
     echo 'Ha sucedido un error inexperado en la conexion de la base de datos<br>';
 }
-$sql = 'SELECT * FROM users_list;';
-if (!$result = mysqli_query($conection, $sql))
+//Consulta que se va a ejecutar
+$query = 'SELECT * FROM users_list;';
+if (!$result = mysqli_query($conection, $query))
     die();
 $rawdata = array();
 $i = 0;
+//Se empaca la información obtenida de la consulta en un Array
 while ($row = mysqli_fetch_assoc($result)) {
     $rawdata[$i] = array_map('utf8_encode', $row);
     $i++;
 }
-$close = mysqli_close($conection);
-if (!$close) {
+if (!mysqli_close($conection)) {
     echo 'Ha sucedido un error inexperado en la desconexion de la base de datos<br>';
 }
+//Se codifica el array en formato JSON
 echo json_encode($rawdata);
 ?>
--- src/main/java/Java_PHP/saveInfo.php
+++ src/main/java/Java_PHP/saveInfo.php
@@ -1,13 +1,15 @@
 <?php
-
+//Credenciales de la base de datos
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";
 $db_name = "example_database";
+//Se crea la conexión con la base d datos
 $db_connection = mysql_connect($db_host, $db_user, $db_password);
 if (!$db_connection) {
     die('No se ha podido conectar a la base de datos');
 }
+//Se empieza a construir el query para insertar los datos
 $query = "INSERT INTO " . $db_name . ".";
 foreach ($_POST as $key => $value) {
     if (($key) == "table_name") {
@@ -23,8 +25,10 @@ foreach ($_POST as $key => $value) {
         $query = $query . "\"" . $value . "\", ";
     }
 }
+//Se corrige el query
 $query = $query . "_";
 $query = str_replace(", _", ")", $query);
+//Se insertan los datos en la base de datos
 mysql_select_db($db_name, $db_connection);
 $retry_value = mysql_query($query, $db_connection);
 if (!$retry_value) {
