a8948f6e6524623f92f93564b7efded3a4c9d7fa
Antony Garcia <Antony Garcia@SonyVaio>
2017-12-25 23:33:33 -0500

    Ejemplo de JavaExecutor1

--- src/main/java/Java/Executor/JavaExecutorExample1.java
+++ src/main/java/Java/Executor/JavaExecutorExample1.java
@@ -1,11 +1,41 @@
 package Java.Executor;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class JavaExecutorExample1 {
 
     public static void main(String[] args) {
-        
-
-
+        ExecutorService exec1 = Executors.newSingleThreadExecutor();
+        exec1.submit(() -> {
+            task1();
+        });
+        ExecutorService exec2 = Executors.newSingleThreadExecutor();
+        exec2.submit(() -> {
+            task2();
+        });
+        ExecutorService exec3 = Executors.newSingleThreadExecutor();
+        exec3.submit(() -> {
+            task3(); 
+        });
     }
 
+    public static void task1() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Task 1: " + i);
+        }
+    }
+
+    public static void task2() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Task 2: " + i);
+        }
+    }
+
+    public static void task3() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Task 3: " + i);
+        }
+    }  
+    
 }
